b07b657027e35d08c5a632483ea211b2ac915a87
Thalita Marques <44007943+Thalita-Marques@users.noreply.github.com>
2018-10-31 19:29:49 -0300

    Add files via upload

---
+++ aula15/11_Heranca/src/controller/Teste.java
@@ -0,0 +1,23 @@
+package controller;
+
+import model.Aluno;
+import model.Professor;
+
+public class Teste {
+	
+	public static void main(String [] args) {
+		
+		Aluno aluno1 = new Aluno ("Thalita Marques1","61250187-0", "150180027-58", "21-9456-1992", "tmarques@gmail.com", "10001", "Programador de Sistemas", 0);
+		Aluno aluno2 = new Aluno ("Thalita Marques2","61250187-0", "150180027-58", "21-9456-1992", "tmarques@gmail.com", "10001", "Programador de Sistemas", 0);
+		Aluno aluno3 = new Aluno ("Thalita Marques3","61250187-0", "150180027-58", "21-9456-1992", "tmarques@gmail.com", "10001", "Programador de Sistemas", 0);
+		
+		
+		System.out.println("Nome:" + aluno1.getNome());
+		System.out.println("Nome:" + aluno2.getNome());
+		System.out.println("Nome:" + aluno3.getNome());
+		System.out.println("São " + Aluno.getNumStudentes() + "alunos registrados");
+		
+		Professor professor = new Professor("Marcelo Roberto Campos", "10054987-07","075598845-75", "21-7793-8574", "mrcampos@senai.rj.gov.br", "901", "TI");
+		System.out.println("Nome do professor:" + professor.getNome());
+	}
+}
