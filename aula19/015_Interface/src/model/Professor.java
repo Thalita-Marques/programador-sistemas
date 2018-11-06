3b1c0b4972f1ef6166874066764ec55aa6b40544
Thalita Marques <44007943+Thalita-Marques@users.noreply.github.com>
2018-11-06 21:33:44 -0200

    Add files via upload

---
+++ aula19/015_Interface/src/model/Professor.java
@@ -0,0 +1,63 @@
+package model;
+
+public  class Professor extends Usuario implements IUsuario {
+
+	private int matriculaProfessor;
+	private String nome;
+	private String especialidade;
+
+	public int getMatriculaProfessor() {
+		return matriculaProfessor;
+	}
+
+	public void setMatriculaProfessor(int matriculaProfessor) {
+		this.matriculaProfessor = matriculaProfessor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+
+	@Override
+	public void mostrar() {
+		System.out.println("Informações do aluno: ");
+		System.out.println("Login: " + super.getLogin());
+		System.out.println("Senha: *****");
+
+		System.out.println("Matrícula: " + matriculaProfessor);
+		System.out.println("Nome: " + nome);
+		System.out.println("Especialidade: " + especialidade);
+	}
+
+	public Professor() {
+		super();
+	}
+
+	public Professor(String login, String senha, int tipo, String nome, String especialidade, int matriculaProfessor) {
+		super(login, senha, tipo);
+		this.nome = nome;
+		this.especialidade = especialidade;
+		this.matriculaProfessor = matriculaProfessor;
+	}
+
+	@Override
+	public boolean validarLogin(String login, String senha) {
+		if (getLogin() .equals(login) && getSenha() .equals(senha)) {
+		      return true;
+	    }
+		return false;
+	}
+
+}
