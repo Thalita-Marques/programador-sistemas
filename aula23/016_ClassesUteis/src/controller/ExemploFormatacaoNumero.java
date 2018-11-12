67ffe767350e1486adeb77cbe485c87f4b094c96
Thalita Marques <44007943+Thalita-Marques@users.noreply.github.com>
2018-11-12 21:30:32 -0200

    Add files via upload

---
+++ aula23/016_ClassesUteis/src/controller/ExemploFormatacaoNumero.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class ExemploFormatacaoNumero {
+
+	public static void main(String[] args) {
+
+		NumberFormat f = NumberFormat.getCurrencyInstance();
+		double valor = 55008.537;
+		System.out.println("Sem formatação: " + valor);
+		System.out.println("Formatado: " + f.format(valor));
+		
+		System.out.println("===========================");
+
+		NumberFormat p = NumberFormat.getPercentInstance();
+		double porcento = 0.05;
+		System.out.println("Sem formatação: " + porcento);
+		System.out.println("Formatado: " + p.format(porcento));
+		
+		System.out.println("===========================");
+
+		NumberFormat n = NumberFormat.getNumberInstance();
+		double numero = 100.50;
+		System.out.println("Sem formatação: " + numero);
+		System.out.println("Formatado: " + n.format(numero));
+		
+		System.out.println("===========================");
+
+		double valor2 = 12000.5;
+
+		DecimalFormat df = new DecimalFormat("R$ ###, ###, ###.00");
+		System.out.println("Sem formatação: " + valor2);
+		System.out.println("Formatado: " + df.format(valor2));
+
+	}
+
+}
