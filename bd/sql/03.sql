d50c816f7de54778ed1b241e3d5bba90b26b7013
Thalita Marques <44007943+Thalita-Marques@users.noreply.github.com>
2018-12-12 21:29:58 -0200

    Add files via upload

---
+++ bd/sql/03.sql
@@ -0,0 +1,32 @@
+use infonote;
+
+CREATE TABLE endereco (
+id_endereco int(11) not null AUTO_INCREMENT,
+logradouro varchar(255),
+numero varchar(50),
+complemento varchar(100),
+bairro varchar(150),
+cidade varchar(150),
+estado varchar(2),
+cep varchar(9),
+id_cliente int(11) not null,
+foreign key (id_cliente) references cliente(id_cliente) ON DELETE CASCADE,
+PRIMARY KEY (id_endereco)
+);
+
+
+
+
+
+
+CREATE TABLE notebook (
+id_notebook int(11) not null AUTO_INCREMENT,
+serialnote varchar(40),
+moldelo varchar(255) not null,
+descricao varchar(255) not null,
+estoque int not null,
+precoUnitario decimal (10, 2) not null,
+figura varchar(10) not null,
+dataCadastro varchar(10)not null,
+PRIMARY KEY (id_notebook)
+)
